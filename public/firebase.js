b5677d430f5b80977c1353cb1e32638a988752ec
ArtGaund <artigaund2210@gmail.com>
2019-11-30 15:39:23 +0530

    adding files

---
+++ public/firebase.js
@@ -0,0 +1,15 @@
+
+//Initialize firebase
+var config = {
+    apiKey: "AIzaSyCA0tFibrFPYizpazr8uH6loNYtsxe6Zkk",
+    authDomain: "darshanraval-d2b7b.firebaseapp.com",
+    databaseURL: "https://darshanraval-d2b7b.firebaseio.com",
+    projectId: "darshanraval-d2b7b",
+    storageBucket: "darshanraval-d2b7b.appspot.com",
+    messagingSenderId: "958297367509"
+  };
+  firebase.initializeApp(config);
+ var current_user="";
+
+
+ 
